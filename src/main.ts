54a46343c738f13b86bfb71108dedef9ddeb3783
Shanika Wijerathna <shanikacj@gmail.com>
2024-11-18 13:04:42 +1300

    Perform tree shaking and use moment js library

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { BaseFunctions, InvokeModalInput } from "./generated/inputTypes.js";
+import moment from "moment";
 
 export class Node implements BaseFunctions {
   async invokeModal(input: InvokeModalInput): Promise<Record<string, any>> {
     console.log(`invokeModal called with: Test1 `, { input });
-    return { success: true };
+    return {
+      success: true,
+      timestamp: moment().format("YYYY-MM-DD HH:mm:ss"),
+    };
   }
 }
